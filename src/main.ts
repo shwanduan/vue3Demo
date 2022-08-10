7239fbb5bd6b7d2ceb20248d32c02a756421b5b1
段振宇 <2577623262@qq.com>
2022-08-10 14:06:15 +0800

    feat :挂载路由

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
+/*
+ * @Descripttion: 
+ * @Author: 段振宇
+ * @version: 
+ * @Date: 2022-08-09 11:37:14
+ * @LastEditors: 段振宇
+ */
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import router from './router'
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
